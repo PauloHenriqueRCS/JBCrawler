88a865d07c4df3ac0bf7085fb53d02a5cd2ffca6
Paulo Henrique Santos <paulohenriquercs@gmail.com>
2018-07-21 02:16:44 -0300

    Recursos

--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
-import express from 'express';
+import express from 'express'
+import MasterWebDriver from './webDriver/masterWebDriver'
 
-const app = express();
+const app = express()
 
-const port = 3000;
+const port = 3000
 
-app.listen(port , () => {
-  console.log('Started on port ', port);
-});
+app.listen(port, () => {
+  console.log('Started on port ', port)
+  MasterWebDriver()
+})
 
 export default app
-
-
-//  import 'chromedriver';
-// // var webdriver = require('selenium-webdriver');
-// // var driver = new webdriver.Builder()
-// //   .forBrowser('chrome')
-// //   .build();
-
-// //   driver.manage().deleteAllCookies()
-
-// //   driver.get('https://google.com')
-// //   driver.close()
-
-// import { Builder, By, Key, until } from 'selenium-webdriver';
-
-// (async function example() {
-//   let driver = await new Builder().forBrowser('chrome').build();
-//   try {
-//     await driver.get('https://www.google.com');
-//     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-//     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-//   } finally {
-//     await driver.quit();
-//   }
-// })();
